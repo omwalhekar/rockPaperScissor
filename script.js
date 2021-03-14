094aa0f74328a8b931f82ab17b91500110184b43
omwalhekar <omwalhekar159@gmail.com>
2021-03-14 14:28:55 +0530

    initial

---
+++ script.js
@@ -0,0 +1,13 @@
+const selectionButtons = document.querySelectorAll("[data-selection");
+
+selectionButtons.forEach(selectionButton => {
+  selectionButton.addEventListener("click", e => {
+    const selectionName = selectionButton.dataset.selection;
+
+    makeSelection(selectionName);
+  });
+});
+
+function makeSelection(selection) {
+  console.log(selection);
+}
